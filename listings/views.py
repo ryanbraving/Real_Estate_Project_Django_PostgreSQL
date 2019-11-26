321a504ef1a1175866a5c3deb0b4742bb6b2232a
Ryan Braving <ryan.braving@gmail.com>
2019-11-27 10:25:08 +1300

    fetch listings and inject into listings template

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from .models import Listing
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    # if you get an error : class 'Listing' has no 'Objects' member
+    # ignore it as it is pylint error, to fix it:
+    # https://stackoverflow.com/questions/45135263/class-has-no-objects-member
+    
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html', {'listing_id': listing_id})
 
 def search(request):
     return render(request, 'listings/search.html')

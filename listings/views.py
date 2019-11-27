ccf78f74134bde3398715060a636bb0abd7fa984
Ryan Braving <ryan.braving@gmail.com>
2019-11-27 21:43:05 +1300

    search form choices done

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
+from .choices import state_choices, price_choices, bedroom_choices
+
 
 # Create your views here.
 
@@ -18,14 +20,23 @@ def index(request):
     paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': paged_listings
+        'listings': paged_listings,
     }
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html', {'listing_id': listing_id})
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+    }
+    return render(request, 'listings/search.html', context)

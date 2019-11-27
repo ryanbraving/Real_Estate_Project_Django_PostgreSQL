4b1d61446b77f2d825ea5dc32ad3f5b5df5ac026
Ryan Braving <ryan.braving@gmail.com>
2019-11-27 13:20:36 +1300

    Pagination added, Listings ordering & Listings filtering added

--- listings/views.py
+++ listings/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator
 
 # Create your views here.
 
+
 def index(request):
-    listings = Listing.objects.all()
+    # listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
     # if you get an error : class 'Listing' has no 'Objects' member
     # ignore it as it is pylint error, to fix it:
     # https://stackoverflow.com/questions/45135263/class-has-no-objects-member
-    
+
+    paginator = Paginator(listings, 6)  # Show 6 listings per page
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
+
 def listing(request, listing_id):
     return render(request, 'listings/listing.html', {'listing_id': listing_id})
+
 
 def search(request):
     return render(request, 'listings/search.html')

d4161fb735c4843b488a46997d62696bd24e8460
Ryan Braving <ryan.braving@gmail.com>
2019-11-29 07:23:01 +1300

    Message Alerts added

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        # Register User
-        return
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     return render(request, 'accounts/register.html')
 
 

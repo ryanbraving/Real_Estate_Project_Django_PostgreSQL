adcd9e3f28d52f15b0da4ddd5cedb9f3b5a90be6
Ryan Braving <ryan.braving@gmail.com>
2019-11-30 13:39:34 +1300

    Added local settings import

--- real_estate_app/settings.py
+++ real_estate_app/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "from@example.com"
 EMAIL_HOST_PASSWORD = "your password"
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass

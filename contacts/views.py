6948a33376beb307c64ea4b6f81b9e957694c1d9
Ryan Braving <ryan.braving@gmail.com>
2019-12-01 11:41:36 +1300

    Email service disabled

--- contacts/views.py
+++ contacts/views.py
@@ -32,14 +32,14 @@ def contact(request):
 
         contact.save()
         # send email
-        send_mail(
-            'Property Listing Inquiry',
-            'Thete has been an inquiry for' + listing +
-            '. Sign into the admin panel for more info',
-            'ryan.braving@gmail.com',
-            [realtor_email, 'ryan.braving@gmail.com'],
-            fail_silently=False,
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'Thete has been an inquiry for' + listing +
+        #     '. Sign into the admin panel for more info',
+        #     'ryan.braving@gmail.com',
+        #     [realtor_email, 'ryan.braving@gmail.com'],
+        #     fail_silently=False,
+        # )
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/' + listing_id)
